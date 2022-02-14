a8ae54a7bf154c4c2fbf2ce2d22b71a3a634ccff
Pan Zhan <pan.zhan@educationhorizons.com>
2022-02-15 04:39:49 +1100

    add style to links in posts

---
+++ app/components/ExLink.tsx
@@ -0,0 +1,4 @@
+export default function ExLink({children}: {children: React.ReactNode}){
+  return <label className="text-sky-400 underline">{children}</label>
+
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  content: ["./app/**/*.tsx"],
+  content: ["./app/**/*.{tsx, mdx}"],
   theme: {
     extend: {},
   },
